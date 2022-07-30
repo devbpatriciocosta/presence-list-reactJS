153d7fbe9b1017359755626f6523d5e54ebe8aec
Bruno Patrício <bpatriciocosta@gmail.com>
2022-07-30 18:23:03 -0300

    Continuando a estilizar!

--- src/components/presenceListPage/PresenceListPage.jsx
+++ src/components/presenceListPage/PresenceListPage.jsx
@@ -2,6 +2,7 @@ import Header from "../header/Header";
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle";
 import Icon from "../subtitle/Icon";
+import styles from '../subtitle/Icon.module.css'
 
 function PresenceListPage () {
   return (
@@ -13,6 +14,9 @@ function PresenceListPage () {
     <div>
       <Title>Lista de presença - Panificação 2022.2</Title>
       <Subtitle>Created by @devbpatriciocosta</Subtitle>
+    </div>
+
+    <div className={styles.icon}>      
       <Icon iconName="github" link="https://github.com/devbpatriciocosta"/>
     </div>
 </>
